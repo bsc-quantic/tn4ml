9e482881e9658427d191a30b80b568a6a6c35bc4
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-07-12 17:50:55 +0200

    Test `embed`

--- test/test_embeddings.py
+++ test/test_embeddings.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 import tnad
+import itertools
 
 
 @pytest.mark.parametrize("x", [0.0, 1.0, -1.0, 0.3, 0.7, 2.0])
@@ -13,3 +14,9 @@ def test_trigonometric(x):
 def test_fourier(x):
     phi: np.ndarray = tnad.embeddings.fourier(x)
     assert np.linalg.norm(phi) == pytest.approx(1.0)
+
+
+@pytest.mark.parametrize("x,embedding", itertools.product((np.random.rand(4) for _ in range(5)), [tnad.embeddings.trigonometric, tnad.embeddings.fourier]))
+def test_embed(x, embedding):
+    phi = tnad.embeddings.embed(x, phi=embedding)
+    assert phi.norm() == pytest.approx(1.0)
