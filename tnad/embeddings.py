d9d519791eb1ab7524350738cb5e2e11f60ec9c4
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-08-08 15:08:03 +0200

    Refactor embeddings into `FeatureMap` class

--- tnad/embeddings.py
+++ tnad/embeddings.py
@@ -1,27 +1,55 @@
 import itertools
 from numbers import Number
-from typing import Callable
 import numpy as np
 import quimb.tensor as qtn
+import abc
 
 
-def trigonometric(x: Number, k: int = 1, dtype=np.float32) -> np.ndarray:
-    return 1 / np.sqrt(k) * np.fromiter((f(np.pi * x / 2**i) for f, i in itertools.product([np.cos, np.sin], range(1, k + 1))), dtype=dtype)
+class FeatureMap:
+    def __init__(self, dtype=np.float32):
+        self.dtype = dtype
+
+    @abc.abstractmethod
+    def dim(self) -> int:
+        pass
+
+    @abc.abstractmethod
+    def __call__(self, x: Number) -> np.ndarray:
+        pass
 
 
-def fourier(x: Number, p: int = 2, dtype=np.float32) -> np.ndarray:
-    return 1 / p * np.fromiter((np.abs(sum((np.exp(1j * 2 * np.pi * k * ((p - 1) * x - j) / p) for k in range(p)))) for j in range(p)), dtype=dtype)
+class trigonometric(FeatureMap):
+    def __init__(self, k: int = 1, **kwargs):
+        assert k >= 1
+
+        self.k = 1
+        super().__init__(self, **kwargs)
+
+    def dim(self) -> int:
+        return self.k * 2
+
+    def __call__(self, x: Number) -> np.ndarray:
+        return 1 / np.sqrt(self.k) * np.fromiter((f(np.pi * x / 2**i) for f, i in itertools.product([np.cos, np.sin], range(1, self.k + 1))), dtype=self.dtype)
 
 
-def embed(x: np.ndarray, /, phi: Callable, **kwargs):
+class fourier(FeatureMap):
+    def __init__(self, p: int = 2, **kwargs):
+        assert p >= 2
+
+        self.p = 2
+        super().__init__(self, **kwargs)
+
+    def dim(self) -> int:
+        return self.p
+
+    def __call__(self, x: Number) -> np.ndarray:
+        return 1 / self.p * np.fromiter((np.abs(sum((np.exp(1j * 2 * np.pi * k * ((self.p - 1) * x - j) / self.p) for k in range(p)))) for j in range(self.p)), dtype=self.dtype)
+
+
+def embed(x: np.ndarray, /, phi: FeatureMap, **mps_opts):
     """Creates a product state from a vector of features `x`."""
     assert x.ndim == 1
 
-    # reshape to expected shape by MPS
-    arrays = [phi(xi, **kwargs) for xi in x]
-    arrays[0] = arrays[0].reshape(1, 2)
-    for i in range(1, len(arrays) - 1):
-        arrays[i] = arrays[i].reshape(1, 1, 2)
-    arrays[-1] = arrays[-1].reshape(1, 2)
+    arrays = [phi(xi).reshape((1, 1, phi.dim)) for xi in x]
 
-    return qtn.MatrixProductState(arrays)
+    return qtn.MatrixProductState(arrays, **mps_opts)
