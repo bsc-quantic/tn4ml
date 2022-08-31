322e11ed706be5caa4d36922f494ddf4345bf04d
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-08-31 12:19:51 +0200

    Fix embed dim in `embed`

--- tnad/embeddings.py
+++ tnad/embeddings.py
@@ -3,13 +3,13 @@ from numbers import Number
 import numpy as np
 import quimb.tensor as qtn
 import abc
-from typing import Callable
 
 
-class FeatureMap:
+class Embedding:
     def __init__(self, dtype=np.float32):
         self.dtype = dtype
 
+    @property
     @abc.abstractmethod
     def dim(self) -> int:
         pass
@@ -19,13 +19,14 @@ class FeatureMap:
         pass
 
 
-class trigonometric(FeatureMap):
+class trigonometric(Embedding):
     def __init__(self, k: int = 1, **kwargs):
         assert k >= 1
 
         self.k = 1
         super().__init__(**kwargs)
 
+    @property
     def dim(self) -> int:
         return self.k * 2
 
@@ -33,13 +34,14 @@ class trigonometric(FeatureMap):
         return 1 / np.sqrt(self.k) * np.fromiter((f(np.pi * x / 2**i) for f, i in itertools.product([np.cos, np.sin], range(1, self.k + 1))), dtype=self.dtype)
 
 
-class fourier(FeatureMap):
+class fourier(Embedding):
     def __init__(self, p: int = 2, **kwargs):
         assert p >= 2
 
         self.p = 2
         super().__init__(**kwargs)
 
+    @property
     def dim(self) -> int:
         return self.p
 
@@ -47,10 +49,10 @@ class fourier(FeatureMap):
         return 1 / self.p * np.fromiter((np.abs(sum((np.exp(1j * 2 * np.pi * k * ((self.p - 1) * x - j) / self.p) for k in range(self.p)))) for j in range(self.p)), dtype=self.dtype)
 
 
-def embed(x: np.ndarray, phi: Callable, **mps_opts):
+def embed(x: np.ndarray, phi: Embedding, **mps_opts):
     """Creates a product state from a vector of features `x`."""
     assert x.ndim == 1
 
-    arrays = [phi(xi).reshape((1, 1, x.ndim)) for xi in x]
+    arrays = [phi(xi).reshape((1, 1, phi.dim)) for xi in x]
 
     return qtn.MatrixProductState(arrays, **mps_opts)
