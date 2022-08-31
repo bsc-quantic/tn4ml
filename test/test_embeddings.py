0f4e6367a075303196c9b9162a304d3f54625471
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-08-31 12:10:52 +0200

    Update embeddings tests to new functor structure

--- test/test_embeddings.py
+++ test/test_embeddings.py
@@ -6,17 +6,19 @@ import itertools
 
 @pytest.mark.parametrize("x", [0.0, 1.0, -1.0, 0.3, 0.7, 2.0])
 def test_trigonometric(x):
-    phi: np.ndarray = tnad.embeddings.trigonometric(x)
+    embedding = tnad.embeddings.trigonometric()
+    phi: np.ndarray = embedding(x)
     assert np.linalg.norm(phi) == pytest.approx(1.0)
 
 
 @pytest.mark.parametrize("x", [0.0, 1.0, -1.0, 0.3, 0.7, 2.0])
 def test_fourier(x):
-    phi: np.ndarray = tnad.embeddings.fourier(x)
+    embedding = tnad.embeddings.fourier()
+    phi: np.ndarray = embedding(x)
     assert np.linalg.norm(phi) == pytest.approx(1.0)
 
 
-@pytest.mark.parametrize("x,embedding", itertools.product((np.random.rand(4) for _ in range(5)), [tnad.embeddings.trigonometric, tnad.embeddings.fourier]))
+@pytest.mark.parametrize("x,embedding", itertools.product((np.random.rand(4) for _ in range(5)), [tnad.embeddings.trigonometric(), tnad.embeddings.fourier()]))
 def test_embed(x, embedding):
     phi = tnad.embeddings.embed(x, phi=embedding)
     assert phi.norm() == pytest.approx(1.0)
