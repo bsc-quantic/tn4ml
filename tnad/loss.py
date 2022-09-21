dad0c8d4fd0d38fb1e83771c9500a46c4dbd1806
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-09-21 10:57:19 +0200

    Refactor `loss` module

--- tnad/loss.py
+++ tnad/loss.py
@@ -1,10 +1,26 @@
-import math
-import quimb
+from numbers import Number
+import quimb.tensor as qtn
+from autoray import do
 
-def loss_miss(phi, P):
-    phi_orig_renamed = phi.reindex({f'k{site}':f'k_{site}' for site in range(phi.nsites)})
-    P_orig_renamed = P.reindex({f'k{site}':f'k_{site}' for site in range(P.nsites)})
-    return math.pow((math.log((phi_orig_renamed.H&P_orig_renamed.H&P&phi)^all) - 1), 2)
 
-def loss_reg(P, alpha):
-    return alpha*max(0, math.log((P.H&P)^all))
+def no_reg(x):
+    return 0
+
+
+def reg_norm_logrelu(P):
+    """Regularization cost using ReLU of the log of the Frobenius-norm of `P`."""
+    return max(0, do("log", P.H & P ^ all))
+
+
+def reg_norm_quad(P):
+    """Regularization cost using the quadratic formula centered in 1 of the Frobenius-norm of `P`."""
+    return do("power", P.H & P ^ all - 1, 2)
+
+
+def loss(model, batch, error=None, reg=no_reg) -> Number:
+    return do("mean", [error(model, sample) for sample in batch]) + reg(model)
+
+
+def error_logquad(P, phi):
+    mps = qtn.tensor_network_apply_op_vec(P, phi)
+    return do("power", do("log", mps.H & mps ^ all) - 1, 2)
