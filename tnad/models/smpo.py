acb1852ca405937894381aec93e82c4c023c19b2
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-10-06 18:59:06 +0200

    Fix normalization of random initialization of SMPO
    Directly dividing one tensor by the norm of the TN, or distributing the norm along the tensors is not numerically stable.
    
    By first canonizing and then normalizing the canonical root, we can normalize in a more stable way.

--- tnad/models/smpo.py
+++ tnad/models/smpo.py
@@ -102,7 +102,11 @@ class SpacedMatrixProductOperator(TensorNetwork1DOperator, TensorNetwork1DFlat, 
         norm = self.norm()
         self.tensors[insert].modify(data=self.tensors[insert].data / norm)
 
-    def rand(n: int, spacing: int, bond_dim: int = 4, phys_dim: Tuple[int, int] = (2, 2), cyclic: bool = False, init_func: str = "uniform", scale: float = 1.0, seed: int = None, **kwargs):
+    def norm(self, **contract_opts):
+        norm = self.conj() & self
+        return norm.contract(**contract_opts) ** 0.5
+
+    def rand(n: int, spacing: int, bond_dim: int = 4, phys_dim: Tuple[int, int] = (2, 2), cyclic: bool = False, init_func: str = "uniform", scale: float = 1.0, seed: int = None, insert=0, **kwargs):
         arrays = []
         for i, hasoutput in zip(range(n), itertools.cycle([True, *[False] * (spacing - 1)])):
             if hasoutput:
@@ -122,6 +126,8 @@ class SpacedMatrixProductOperator(TensorNetwork1DOperator, TensorNetwork1DFlat, 
                 arrays.append(qu.gen.rand.randn(shape, dist=init_func, scale=scale))
         mpo = SpacedMatrixProductOperator(arrays, **kwargs)
         mpo.compress(form="flat", max_bond=bond_dim)  # limit bond_dim
+        mpo.canonize(insert)
+        mpo.normalize(insert)
         return mpo
 
     @property
