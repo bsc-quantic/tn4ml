9dbdfabe63f3a45aa11f39cd64c6ffc6a785a6a0
epuljak <ema.puljak@cern.ch>
2022-10-20 14:38:24 +0200

    Separate `get_total_grad_and_loss` into `get_total_grad` and `get_total_loss`

--- tnad/models/util.py
+++ tnad/models/util.py
@@ -1,8 +1,9 @@
 from tnad.embeddings import embed
 from tnad.gradients import gradient_miss, gradient_reg
 
-def get_total_grad_and_loss(P, tensor, data, loss, batch_size=None, alpha=0):
+def get_total_grad(P, tensor, data, loss, batch_size=None, alpha=0):
     """ data = array of MPSs """
+    history = dict()
     
     P_rem = P.copy(deep=True)
     
@@ -11,16 +12,11 @@ def get_total_grad_and_loss(P, tensor, data, loss, batch_size=None, alpha=0):
     P_rem.delete(site_tag, which="any")
 
     # paralelize
-    grad_miss = []; loss_value=0
+    grad_miss = []
     for i, phi in enumerate(data):
         # grad per sample
         grad_miss.append(gradient_miss(phi, P, P_rem, [tensor]))
-        # loss per sample
-        loss_value += loss['error'](P, phi)
    
-    # total loss
-    total_loss = (1/batch_size)*(loss_value) + alpha*loss['reg'](P)
-    
     # gradient of error part
     grad_miss = sum(grad_miss)
     grad_miss.drop_tags()
@@ -31,6 +27,26 @@ def get_total_grad_and_loss(P, tensor, data, loss, batch_size=None, alpha=0):
         grad_regular.drop_tags()
         grad_regular.add_tag(site_tag)
     # total gradient
+    history['grad_miss'] = (1/batch_size)*(grad_miss)
+    history['grad_reg'] = grad_regular
     total_grad = (1/batch_size)*(grad_miss) + grad_regular
     
-    return total_grad, total_loss
+    return total_grad, history
+
+def get_total_loss(P, data, loss, batch_size=None, alpha=0):
+    """ data = array of MPSs """
+    history = dict()
+    
+    # paralelize
+    loss_value=0
+    for i, phi in enumerate(data):
+        # loss per sample
+        loss_value += loss['error'](P, phi)
+   
+    # total loss
+    history['loss_miss'] = (1/batch_size)*(loss_value)
+    history['loss_reg'] = loss['reg'](P)
+    
+    total_loss = (1/batch_size)*(loss_value) + alpha*loss['reg'](P)
+    
+    return total_loss, history
