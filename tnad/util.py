fc5a511b167be8afc7e760436ca89fe01d641083
epuljak <ema.puljak@cern.ch>
2022-11-15 17:23:00 +0100

    Add EarlyStopping class

--- tnad/util.py
+++ tnad/util.py
@@ -1,4 +1,5 @@
 import re
+from typing import NamedTuple
 
 def return_digits(array):
     digits=[]
@@ -8,3 +9,10 @@ def return_digits(array):
             if t.isdigit(): digits.append(int(t))
             else: continue
     return digits
+
+class EarlyStopping(NamedTuple):
+    """ Variation of EarlyStopping class from Tensorflow """
+    monitor: str # name of metric to be monitored
+    min_delta: float # minimum change in the monitored quantity to qualify as an improvement
+    patience: int # number of epochs for tracking the metric, if no improvement after training is stopped
+    mode: str # 'min' - minimization, 'max' - maximization of objective function
