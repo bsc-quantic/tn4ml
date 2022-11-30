c8c1d129d1992ee76802dc607b616194daa2ce56
epuljak <ema.puljak@cern.ch>
2022-12-01 00:35:35 +0100

    Change documentation.

--- tnad/loss.py
+++ tnad/loss.py
@@ -12,33 +12,82 @@ def no_reg(x):
     return 0
 
 def reg_norm_logrelu(P):
-    """Regularization cost using ReLU of the log of the Frobenius-norm of `P`."""
+    """Regularization cost using ReLU of the log of the Frobenius-norm of `P`.
+    
+    Parameters
+    ----------
+    P : :class:`tnad.models.smpo.SpacedMatrixProductOperator`
+        Spaced Matrix Product Operator
+    
+    Returns
+    -------
+    float
+    """
     return do("maximum", 0.0, do("log", P.H.apply(P)))
 
 def reg_norm_quad(P):
-    """Regularization cost using the quadratic formula centered in 1 of the Frobenius-norm of `P`."""
+    """Regularization cost using the quadratic formula centered in 1 of the Frobenius-norm of `P`.
+    
+    Parameters
+    ----------
+    P : :class:`tnad.models.smpo.SpacedMatrixProductOperator`
+        Spaced Matrix Product Operator
+    
+    Returns
+    -------
+    float
+    """
     return do("power", do("add", P.H.apply(P), -1.0), 2)
 
 def error_logquad(P, data):
+    """Example of error calculation when applying :class:`tnad.models.smpo.SpacedMatrixProductOperator` `P` to `data`.
+    
+    Parameters
+    ----------
+    P : :class:`tnad.models.smpo.SpacedMatrixProductOperator`
+        Spaced Matrix Product Operator
+    data: :class:`numpy.ndarray`
+        Input data.
+    Returns
+    -------
+    float
+    """
     mps = P.apply(data)
     return do("power", do("add", do("log", mps.H & mps ^ all), -1.0), 2)
 
 def error_quad(P, data):
+    """Example of error calculation when applying :class:`tnad.models.smpo.SpacedMatrixProductOperator` `P` to `data`.
+    
+    Parameters
+    ----------
+    P : :class:`tnad.models.smpo.SpacedMatrixProductOperator`
+        Spaced Matrix Product Operator
+    data: :class:`numpy.ndarray`
+        Input data.
+    Returns
+    -------
+    float
+    """
     mps = P.apply(data)
     return do("power", do("add", mps.H & mps ^ all, -1.0), 2)
 
 def loss(model, data, error: Callable = error_logquad, reg: Callable = no_reg, embedding: Optional[Embedding] = None) -> Number:
-    """
-    Example of Loss function.
+    """Example of Loss function with calculation of error on input data and regularization.
     
-    Args
-       model: Model
-       data: Data used for computing the loss value. `numpy.ndarray`.
-       error: Function of error calculation. 
-       reg: Function of regularization value calculation.
-       embedding: Data embedding function. `embeddings.Embedding` instance.
+    Parameters
+    ----------
+    model : :class:`tnad.models.Model`
+    data: :class:`numpy.ndarray`
+        Data used for computing the loss value.
+    error: function
+        Function for error calculation. 
+    reg: function
+        Function for regularization value calculation.
+    embedding: :class:`tnad.embeddings.Embedding`
+        Data embedding function.
     
-    Return
-        Number
+    Returns
+    -------
+    float
     """
     return do("mean", [error(model, embed(sample, embedding)) for sample in data] if embedding else error(model, data)) + reg(model)
