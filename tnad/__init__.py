a8e0afd58edc26d59683cf4af5e495a2754bada2
epuljak <ema.puljak@cern.ch>
2023-01-24 11:19:07 +0100

    Clean imports

--- tnad/__init__.py
+++ tnad/__init__.py
@@ -1,2 +1,27 @@
-from . import embeddings, loss, models
-from .strategy import *
+import warnings
+
+from .embeddings import (
+    Embedding,
+    trigonometric,
+    fourier,
+    embed
+)
+
+from .loss import (
+    no_reg,
+    reg_norm_logrelu,
+    reg_norm_quad,
+    error_logquad,
+    error_quad,
+    loss
+)
+
+from .strategy import (
+    Strategy,
+    Sweeps,
+    Global
+)
+
+from .util import (
+    EarlyStopping
+)
