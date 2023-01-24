b8d72665f3fad08c331626d176a9409d49dc4346
epuljak <ema.puljak@cern.ch>
2023-01-24 11:17:42 +0100

    Adding setup description

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup
 
-setup()
+setup(
+    name='tnad',
+    description='Tensor Networks for Anomaly Detection',
+    author='Sergio Sanchez Ramirez, Ema Puljak, Sergi Masot Llima, Jofre Valles Muns',
+)
