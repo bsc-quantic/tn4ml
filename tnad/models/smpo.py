d5c90edbfd44bf521fa30f582a9c31d656f0efae
epuljak <ema.puljak@cern.ch>
2023-03-15 20:57:39 +0100

    Fix `rand_orthogonal` in `smpo.py`

--- tnad/models/smpo.py
+++ tnad/models/smpo.py
@@ -88,6 +88,7 @@ class SpacedMatrixProductOperator(TensorNetwork1DOperator, TensorNetwork1DFlat, 
         dims = [x.ndim for x in arrays]
         # if dims.count(4) == 0:
         #     raise ValueError('There is only one output index --> spacing >= L. Consider changing the value.')
+        
         # enable spacing ==  (to have one output)
         if dims.count(4) == 0:
             self._spacing = self.L
@@ -271,29 +272,29 @@ class SpacedMatrixProductOperator(TensorNetwork1DOperator, TensorNetwork1DFlat, 
             raise NotImplementedError()
 
         arrays = []
-        for i, hasoutput in zip(range(n), itertools.cycle([True, *[False] * (spacing - 1)])):
-            if i < n // 2:
-                j = i
+        for i, hasoutput in zip(range(1, n+1), itertools.cycle([True, *[False] * (spacing - 1)])):
+            if i > n // 2:
+                j = (n + 1 - abs(2*i - n - 1)) // 2
             else:
-                j = (n - 1 - abs(2*i - n)) // 2
-
-            chil = min(bond_dim, phys_dim[0] ** (j-1) * phys_dim[1] ** (1 + j // spacing))
-            chir = min(bond_dim, phys_dim[0] ** j * phys_dim[1] ** (1 + (j+1) // spacing))
+                j = i
+            
+            chil = min(bond_dim, phys_dim[0] ** (j-1) * phys_dim[1] ** ((j-1)//spacing))
+            chir = min(bond_dim, phys_dim[0] ** (j) * phys_dim[1] ** ((j)//spacing))  
 
             if i > n // 2:
                 (chil, chir) = (chir, chil)
 
             if hasoutput:
-                if i == 0:
+                if i == 1:
                     shape = (chir, *phys_dim)
-                elif i == n - 1:
+                elif i == n:
                     shape = (chil, *phys_dim)
                 else:
                     shape = (chil, chir, *phys_dim)
             else:
-                if i == 0:
+                if i == 1:
                     shape = (chir, phys_dim[0])
-                elif i == n - 1:
+                elif i == n:
                     shape = (chil, phys_dim[0])
                 else:
                     shape = (chil, chir, phys_dim[0])
@@ -303,8 +304,8 @@ class SpacedMatrixProductOperator(TensorNetwork1DOperator, TensorNetwork1DFlat, 
             else:
                 A = gramschmidt(qu.gen.rand.randn([shape[0], np.prod(shape[1:])], dist=init_func, scale=scale))
             arrays.append(np.reshape(A, shape))
-
-        arrays[0] /= np.sqrt(phys_dim[0])
+        
+        arrays[0] /= np.sqrt(min(bond_dim, phys_dim[0]))
 
         mpo = SpacedMatrixProductOperator(arrays, **kwargs)
         return mpo
