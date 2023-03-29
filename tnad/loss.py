dce5e59e12b37e5f04a5ea1369e7138e616ba589
epuljak <ema.puljak@cern.ch>
2023-03-29 17:36:16 +0200

    Add `cross_entropy` loss

--- tnad/loss.py
+++ tnad/loss.py
@@ -3,6 +3,9 @@ from typing import Callable, Optional
 
 import quimb.tensor as qtn
 from autoray import do
+import numpy as np
+import jax.numpy as jnp
+import math
 
 from .embeddings import Embedding, embed
 
@@ -87,7 +90,32 @@ def error_distance_to_origin(P, data):
     mps = P.apply(data)
     return - (mps.H & mps ^ all)
 
-def loss(model, data, error: Callable = error_logquad, reg: Callable = no_reg, embedding: Optional[Embedding] = None) -> Number:
+def softmax(z):
+    sum_z = do("sum", jnp.asarray([do("power", math.e, z_j) for z_j in z]))
+    return jnp.asarray([do("power", math.e, z_i)/sum_z for z_i in z])
+
+def error_cross_entropy(P, data, mps_target):
+    """Example of supervised error calculation when applying :class:`tnad.models.smpo.SpacedMatrixProductOperator` `P` to `data`.
+    
+    Parameters
+    ----------
+    P : :class:`tnad.models.smpo.SpacedMatrixProductOperator`
+        Spaced Matrix Product Operator
+    data: :class:`quimb.tensor.MatrixProductState`
+        Input mps.
+    mps_target: :class:`numpy.ndarray`
+        Target data.
+    Returns
+    -------
+    float
+    """
+    mps = P.apply(data)
+    class_vector = mps.tensors[0].data
+    class_vector = do("reshape", class_vector, (5,))
+    prob_dist = softmax(class_vector)
+    return - do("sum", mps_target * do("log", do("power", prob_dist, 2)))
+
+def loss_fn(model, data, error: Callable = error_logquad, reg: Callable = no_reg, embedding: Optional[Embedding] = None) -> Number:
     """Example of Loss function with calculation of error on input data and regularization.
 
     Parameters
