ef634e029477b02920d0e42999cb441cd50c8810
epuljak <ema.puljak@cern.ch>
2023-04-12 17:15:53 +0200

    Fix project `setuptools`

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='tn4ml',
-    description='Tensor Networks for Machine Learning',
-    author='Sergio Sanchez Ramirez, Ema Puljak, Sergi Masot Llima, Jofre Valles Muns',
-)
