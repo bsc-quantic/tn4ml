2e9b9741ff97bd10311a71e7b392c0fd874d06ce
Ema Puljak <ema.puljak@cern.ch>
2023-09-28 21:13:08 +0200

    Edit `Sweeps` strategy

--- tn4ml/strategy.py
+++ tn4ml/strategy.py
@@ -48,7 +48,6 @@ class Strategy:
         """
         pass
 
-
 class Sweeps(Strategy):
     """DMRG-like local optimization.
 
@@ -61,32 +60,42 @@ class Sweeps(Strategy):
     split_opts: optional
         Additional args passed to ``model.split_tensor()``.
     """
-
+    # TODO still not fully functional for grouping=2
     def __init__(self, grouping: int = 2, two_way=True, split_opts={"cutoff": 0.}, **kwargs):
+        # TODO right now only support grouping <= 2
+        if grouping > 2:
+            raise ValueError(f"{grouping=} > 2")
         self.grouping = grouping
         self.two_way = two_way
         self.split_opts = split_opts
         super().__init__(**kwargs)
 
     def iterate_sites(self, model):
-        for i in model.sites[: len(model.sites) - self.grouping + 1]:
-            yield tuple(model.sites[i + j] for j in range(self.grouping))
+        if self.grouping == 1:
+            sites = [site for site in model.sites]
+            
+            for site in list(reversed(model.sites[:-1])):
+                sites.append(site)
+            for site in sites:
+                yield site
+        else:
+            for i in model.sites[: len(model.sites) - self.grouping + 1]:
+                yield tuple(model.sites[i + j] for j in range(self.grouping))
 
     def prehook(self, model, sites):
-        if self.grouping > 2:
-            raise ValueError(f"{self.grouping=} > 2")
+        if self.grouping == 1:
+            return
+        
         model.canonize(sites)
         
         # remembed bond_dim
-        self.bond_dim_split = model.bond_size(sites[0], sites[1])
+        #self.bond_dim_split = model.bond_size(sites[0], sites[1])
         sitetags = tuple(model.site_tag(site) for site in sites)
         model.contract_tags(sitetags, inplace=True)
-        # print('------ Model after prehook ------')
-        # for i, t in enumerate(model.tensors):
-        #     if i>79:
-        #         print(t)
 
     def posthook(self, model, sites):
+        if self.grouping == 1:
+            return
         sitetags = [model.site_tag(site) for site in sites]
         tensor = model.select_tensors(sitetags, which="all")[0]
         # normalize
@@ -94,23 +103,20 @@ class Sweeps(Strategy):
             tensor.normalize(inplace=True)
 
         # split tensor
-        # TODO right now only support grouping <= 2
-        if self.grouping > 2:
-            raise ValueError(f"{self.grouping=} > 2")
-
         sitel, siter = sites
         if isinstance(model, qtn.MatrixProductState):
             site_ind_prefix = model.site_ind_id.rstrip("{}")
             vindl = [f'{site_ind_prefix}{sitel}'] + ([model.bond(sitel - 1, sitel)] if sitel > 0 else [])
-            model.split_tensor(sitetags, left_inds=[*vindl], max_bond=model.max_bond(), method='qr', **self.split_opts)
+            model.split_tensor(sitetags, left_inds=[*vindl], max_bond=model.max_bond(), **self.split_opts)
         else:
             site_ind_prefix = model.upper_ind_id.rstrip("{}")
 
             vindl = [model.upper_ind(sitel)] + ([model.bond(sitel - 1, sitel)] if sitel > 0 else [])
             # vindr = [model.upper_ind(siter)] + ([model.bond(siter, siter + 1)] if siter < model.nsites - 1 else []) + ([model.lower_ind(siter)] if model.tensors[siter].ndim == 4 or (siter==model.L-1 and model.tensors[siter].ndim == 3) else [])
-            lower_ind = [f"{model.lower_ind_id[:-2]}{sitel}"] if f"{model.lower_ind_id[:-2]}{sitel}" in list(model.lower_inds) else []
-            #print(f'======Left inds for split: {[*vindl, *lower_ind]}')
+            lower_ind_prefix = model.lower_ind_id.rstrip("{}")
+            lower_ind = [f"{lower_ind_prefix}{sitel}"] if f"{lower_ind_prefix}{sitel}" in list(model.lower_inds) else []
             model.split_tensor(sitetags, left_inds=[*vindl, *lower_ind], max_bond=self.bond_dim_split, **self.split_opts)
+        
         # fix tags
         for tag in sitetags:
             for tensor in model.select_tensors(tag):
@@ -118,10 +124,6 @@ class Sweeps(Strategy):
                 site_ind = next(filter(lambda ind: ind.removeprefix(site_ind_prefix).isdecimal(), tensor.inds))
                 site = site_ind.removeprefix(site_ind_prefix)
                 tensor.add_tag(model.site_tag(site))
-        # print('--------Model after posthook!-------')
-        # for i, t in enumerate(model.tensors):
-        #     if i>79:
-        #         print(t)
 
 
 class Global(Strategy):
@@ -138,4 +140,3 @@ class Global(Strategy):
         # renormalize
         if self.renormalize:
             model.normalize(inplace=True)
-            print(f'renormalized')
