5a9cea6e9a3e8e64eef16b497d43555542e916ed
Ema Puljak <ema.puljak@cern.ch>
2024-02-09 18:28:35 +0100

    Updated model `LoTeNet`

--- tn4ml/models/lotenet.py
+++ tn4ml/models/lotenet.py
@@ -101,7 +101,8 @@ def unsqueeze_image(image, S=3):
     new_dims.append(n_features)
 
     reshaped_image = image.reshape(tuple(new_dims))
-    averaged_image = torch.sum(reshaped_image, -1)
+    averaged_image = torch.mean(reshaped_image, -1)
+    averaged_image = (averaged_image - torch.min(averaged_image))/(torch.max(averaged_image) - torch.min(averaged_image))
     averaged_image = torch.unsqueeze(averaged_image, -1)
     return averaged_image
 
@@ -132,7 +133,7 @@ def unsqueezed_dimensions(input_dims, S=3):
 
 class loTeNet(torch.nn.Module):
 
-    #input_dim: dimensionality of input image - each dimension is power of self.kernel
+    # input_dim: dimensionality of input image - each dimension is power of self.kernel
     # output_dim: number of classes
     # bond_dim: bond dimension
     # kernel: kernel value - both padding and stride, for now only one value for all layers
@@ -167,7 +168,7 @@ class loTeNet(torch.nn.Module):
 
         skeletons = []
         params = []
-        #BNs = [] # Batch Normalization layers
+        BNs = [] # Batch Normalization layers
         self.n_layers = 0
         while True:
             # MPS with output index
@@ -179,8 +180,6 @@ class loTeNet(torch.nn.Module):
             # gather params and skeleton of each MPS
             param_i = []; skeleton_i = []
             for n_pixel, layer in enumerate(layer_i):
-                for t in layer.tensors:
-                    print(t.data)
                 param, skeleton = qtn.pack(layer)
 
                 param_dict = {}
@@ -190,21 +189,19 @@ class loTeNet(torch.nn.Module):
                 skeleton_i.append(skeleton)
             skeletons.append(skeleton_i)
             params.append(torch.nn.ParameterList(param_i))
-            #BNs.append(torch.nn.BatchNorm1d(self.virtual_dim, affine=True, dtype = torch.float32, track_running_stats=True))
+            BNs.append(torch.nn.BatchNorm1d(self.virtual_dim, affine=True, dtype = torch.float32, track_running_stats=True))
 
             # find shape of output image
-            output_shape = (N_i, self.output_dim)
+            output_shape = (N_i, self.virtual_dim)
             unsqueezed_dim = unsqueezed_dimensions(output_shape, self.S)
             # find new N_i -> number of MPSs in next layer
             new_dim, feature_dim = squeeze_dimensions(unsqueezed_dim, self.kernel)
             N_i = math.prod(new_dim)
-            
-            self.n_layers += 1
-            
+
             # if we came to last layer where number of MPS = 1 --> finish 
             if N_i == 1:
-                
-                #self.BNs = BNs
+
+                self.BNs = BNs
                 # last MPS with output_dim = number of classes
                 layer = SpacedMatrixProductOperator.rand_init(n=feature_dim,\
                                                                 spacing=feature_dim,\
@@ -212,17 +209,19 @@ class loTeNet(torch.nn.Module):
                                                                 phys_dim=(self.phys_dim_input, self.output_dim),\
                                                                 init_method='random_eye')
                 param, skeleton = qtn.pack(layer)
-                param_dict = {f'param_{i}_mps{1}_nL{self.n_layers}': torch.nn.Parameter(torch.tensor(data, dtype=torch.float32), requires_grad=True) for i, data in param.items()}
+                param_dict = {f'param_{i}_mps_{0}_nL{self.n_layers}': torch.nn.Parameter(torch.tensor(data, dtype=torch.float32), requires_grad=True) for i, data in param.items()}
                 params.append(torch.nn.ParameterList([torch.nn.ParameterDict(param_dict)]))
                 skeletons.append([skeleton])
                 
                 # save params and skeletons in Module
                 self.params = torch.nn.ParameterList(params)
                 self.skeletons = skeletons
-
-                break
+                self.n_layers += 1
+                return
             elif N_i < 1:
                 ValueError("Last layer needs to have N_i = 1.")
+            
+            self.n_layers += 1
 
     def pass_per_layer(self, input_image, params_i, skeleton_i):
 
@@ -242,6 +241,7 @@ class loTeNet(torch.nn.Module):
             
             # MPS + MPS_with_output = vector
             output = mps_model.apply(mps_input)^all
+
             vector_outputs.append(output.data.reshape((self.virtual_dim, )))
         vector_outputs = torch.stack(vector_outputs, dim=0)
         
@@ -250,79 +250,20 @@ class loTeNet(torch.nn.Module):
     def pass_final_layer(self, input_image, params, skeleton):
         squeezed_image = squeeze_image(input_image, k=self.kernel)
         squeezed_image = squeezed_image.reshape(math.prod(squeezed_image.shape[:-1]), squeezed_image.shape[-1])
-        
+
         # embed input image
         mps_input = embeddings.embed(squeezed_image[0], self.embedding_input, pytorch=True) # 0 because its the only pixel
+        
         # return params to quimb
         params = {int(key.split('_')[1]): value for key, value in params.items()}
-    
         # unpack model from params and skeleton
         mps_model = qtn.unpack(params, skeleton)
 
         # MPS + MPS_with_output = vector
         output = mps_model.apply(mps_input)^all
         output = output.data.reshape((self.output_dim, ))
-        #output = torch.nn.Softmax()(output)
-        return output
-    
-    # def forward_per_sample(self, input_image):
-    #     """
-    #     Forward pass - per sample
-
-    #     Parameters
-    #     ----------
-    #     image : torch.tensor
-    #         2D or 3D image with additional Channel dimension (H,W,C) or (H,W,D,C)
-
-    #     Returns
-    #     -------
-    #     torch.tensor
         
-    #     """
-    #     # FORWARD PASS
-
-    #     for n_layer, (params_i, skeleton_i) in enumerate(zip(self.params, self.skeletons)):
-
-    #         # if we came to last layer where number of MPS = 1 --> finish     
-    #         if n_layer == self.n_layers:
-    #             squeezed_image = squeeze_image(input_image, k=self.kernel)
-    #             squeezed_image = squeezed_image.reshape(math.prod(squeezed_image.shape[:-1]), squeezed_image.shape[-1])
-                
-    #             # embed input image
-    #             mps_input = embeddings.embed(squeezed_image[0], self.embedding_input, pytorch=True) # 0 because its the only pixel
-                
-    #             # return params to quimb
-    #             params = {int(key.split('_')[1]): value for key, value in params_i[0].items()}
-            
-    #             # unpack model from params and skeleton
-    #             mps_model = qtn.unpack(params, skeleton_i[0])
-
-    #             # MPS + MPS_with_output = vector
-    #             output = mps_model.apply(mps_input)^all
-    #             output = output.data.reshape((self.output_dim, ))
-    #             output = torch.nn.Softmax()(output)
-    #             return output
-            
-    #         squeezed_image = squeeze_image(input_image, k=self.kernel)
-    #         squeezed_image = squeezed_image.reshape(math.prod(squeezed_image.shape[:-1]), squeezed_image.shape[-1])
-
-    #         vector_outputs = []
-    #         for n_pixel, (params, skeleton) in enumerate(zip(params_i, skeleton_i)):
-    #             # embedded input image
-    #             mps_input = embeddings.embed(squeezed_image[n_pixel], self.embedding_input, pytorch=True)
-                
-    #             # return params to quimb
-    #             params = {int(key.split('_')[1]): value for key, value in params.items()}
-                
-    #             # unpack model from params and skeleton
-    #             mps_model = qtn.unpack(params, skeleton)
-                
-    #             # MPS + MPS_with_output = vector
-    #             output = mps_model.apply(mps_input)^all
-    #             vector_outputs.append(output.data.reshape((self.virtual_dim, )))
-
-    #         # reshape to image-like for next layer
-    #         input_image = unsqueeze_image(torch.stack(vector_outputs, dim=0), self.S)
+        return output
 
     def forward(self, x):
         """
@@ -340,28 +281,25 @@ class loTeNet(torch.nn.Module):
         
         """
 
-        # for i, (name, param) in enumerate(self.named_parameters()):
-        #     if i < 2:
-        #         print(param)
-
         batch_size = x.shape[0]
         for n_layer, (params_i, skeleton_i) in enumerate(zip(self.params, self.skeletons)):
+            #print(f'LAYER = {n_layer}')
             if n_layer == self.n_layers:
                 x = vmap(self.pass_final_layer, in_dims=(0, None, None))(x, params_i[0], skeleton_i[0])
                 return x
 
             x = vmap(self.pass_per_layer, in_dims=(0, None, None))(x, params_i, skeleton_i)
-
+            
             x_dims = x.shape[1:]
 
             # reshape
-            #x = x.reshape(batch_size, x_dims[-1], x_dims[0])
+            x = x.reshape(batch_size, x_dims[-1], x_dims[0])
 
             # batch norm
-            #x = self.BNs[n_layer](x) # size = (B, n_features, n_mps)
+            x = self.BNs[n_layer](x) # size = (B, n_features, n_mps)
 
             # reshape back
-            #x = x.reshape(batch_size, x_dims[0], x_dims[-1])
+            x = x.reshape(batch_size, x_dims[0], x_dims[-1])
 
             # reshape to image-like for next layer
             x = vmap(unsqueeze_image, in_dims=(0, None))(x, self.S)
