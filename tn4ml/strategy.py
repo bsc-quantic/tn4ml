c86a85f0c3046704a64a020d6ed5611da5dc5ff7
Ema Puljak <ema.puljak@cern.ch>
2025-02-19 09:24:47 +0100

    Correct canonization in `sweeps`

--- tn4ml/strategy.py
+++ tn4ml/strategy.py
@@ -140,7 +140,7 @@ class Sweeps(Strategy):
         if self.grouping > 2:
             raise NotImplementedError('Not implememented for grouping > 2.')
         
-        model.canonicalize(sites)
+        model.canonicalize(sites, inplace=True)
         
         # remembed bond_size and bond_name
         self.bond_dim_split = model.bond_size(sites[0], sites[1])
